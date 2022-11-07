a38639491378f0e278cf4830f50cb3f602a99343
Blackport <bab1228@uncw.edu>
2022-11-07 18:11:58 -0500

    added code

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import socket
-import time
+import sys
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.settimeout(5)
-sock.connect(('www.google.com', 80))
-sock.send(b'GET / HTTP/1.1\r\nHost:www.google.com\r\n\r\n')
+def request_site(host, port, _type, file=''):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
+    sock.connect((host, port))
+    sock.send(f'{_type} / HTTP/1.1\r\nHost:{host}{("/" + file) * bool(file)}\r\n\r\n'.encode())
 
-response = b""
-while True:
-    try:
-        chunk = sock.recv(4096)
-        if len(chunk) == 0:     # No more data received, quitting
+    response = b""
+    while True:
+        try:
+            chunk = sock.recv(4096)
+            if len(chunk) == 0:     # No more data received, quitting
+                break
+            response += chunk
+        except TimeoutError:
             break
-        response += chunk
-    except TimeoutError:
-        break
 
-# print(response)
-with open('test.html', 'wb') as f:
-    f.write(response)
-# print(response.decode())
-sock.close()
+    segments = response.split(b'\r\n\r\n')
+    header = segments[0]
+    body = b''
+    if len(segments) > 1:
+        for segment in segments[1:]:
+            body += segment
+
+    with open('test.html', 'wb') as f:
+        f.write(body)
+
+    sock.close()
+
+def main():
+    args = sys.argv
+    if len(args) not in (3, 4):
+        raise ValueError('Invalid amount of arguments supplied: Must supply (host, port_number, type, [filename])')
+    request_site(*args)
+
+
