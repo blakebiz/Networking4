85b79ac23fa6e1fd7ee89689823e6862e9927602
Blackport <bab1228@uncw.edu>
2022-12-05 17:48:20 -0500

    added documentation

--- client.py
+++ client.py
@@ -1,19 +1,19 @@
-import json
 import os
 import socket
 import sys
-import time
 
 
 def request_site(host: str, port: int, _type: str, file: str = '') -> None:
-    '''
+    """
     :param host: server ip or domain to request to
     :param port: server port
     :param _type: GET or PUT request
     :param file: name of file to request, if requesting one
 
     Performs a GET or PUT request to a given host and port
-    '''
+    """
+    # make sure proper type was given
+    _type = _type.upper()
     assert _type in ('GET', 'PUT')
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(10)
@@ -26,13 +26,14 @@ def request_site(host: str, port: int, _type: str, file: str = '') -> None:
 
 
 def receive_file(sock: socket.socket, file: str) -> None:
-    '''
+    """
     :param sock: active socket to read from
     :param file: name to save the file as
 
     Reads the data from the socket and stores it in the given filename
-    '''
+    """
     response = b""
+    # read all the data being sent and store it in response
     while True:
         try:
             chunk = sock.recv(4096)
@@ -42,32 +43,37 @@ def receive_file(sock: socket.socket, file: str) -> None:
         except socket.timeout:
             print('timeout')
             break
-
+    # separate body and header
     segments = response.split(b'\r\n\r\n')
     header = segments[0]
     body = b''
+    # if seperator found in body data, append to body
     if len(segments) > 1:
         for segment in segments[1:]:
             body += segment
     print('header:', header)
+    # make crec (client received) directory if it doesn't exist
     if not os.path.isdir('crec'):
         os.mkdir('crec')
+    # save data in crec directory with given filename
     if file:
         print(f'file has been saved as: crec/{file}')
         with open('crec/' + file, 'wb') as f:
             f.write(body)
     else:
         print('body:', body)
+    # close the connection
     sock.close()
 
 
 def send_file(sock: socket.socket, file: str) -> None:
-    '''
+    """
     :param sock: active socket to send to
     :param file: name of file to send
 
     sends the data from the given file to the given socket
-    '''
+    """
+    # try to find the file given, if not error, if found send data
     try:
         with open(file, 'rb') as f:
             body_data = f.read()
@@ -79,10 +85,11 @@ def send_file(sock: socket.socket, file: str) -> None:
 
 
 def main():
-    '''
+    """
     takes input from command line, or requests input if not given, and calls the appropriate function
-    '''
+    """
     args = sys.argv[1:]
+    # if unexpected amount of arguments given, prompt for them
     if len(args) not in (3, 4):
         print('arguments not supplied properly in command line, must be manually entered.')
         args = [
--- server.py
+++ server.py
@@ -2,8 +2,7 @@ import json
 import os
 import socket
 import re
-import threading
-import time
+from typing import Union, List
 
 
 class Server:
@@ -17,17 +16,32 @@ class Server:
         self.extended_timeout = extended_timeout
         self.host, self.port = host, port
 
-
-    def accept_connection(self):
+    def accept_connection(self) -> None:
+        """
+        waits for a connection to be made from a client, then responds
+        """
+        # create new socket with reusable address option
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # bind socket to given host/port
         self.sock.bind((self.host, self.port))
+        # allow the socket to accept 1 connection at most
         self.sock.listen(1)
+        # wait for the client to connect
         self.sock, _ = self.sock.accept()
+        # get request sent by the client
         data = self.get_response(self.sock)
+        # respond to the given request
         self.respond(data)
 
-    def get_response(self, sock, extend=False):
+    def get_response(self, sock: socket.socket, extend: bool = False) -> bytes:
+        """
+        :param sock: active socket to listen for a response from
+        :param extend: whether to extend the timeout or not
+        :return: the response from the given socket
+
+        listens for a response from a given socket and returns the response
+        """
         if extend:
             sock.settimeout(self.extended_timeout)
         response = b""
@@ -42,53 +56,87 @@ class Server:
             sock.settimeout(self.default_timeout)
         return response
 
-    def respond(self, data):
+    def respond(self, data: bytes) -> None:
+        """
+        :param data: The data to respond to
+
+        reads the given data and sends the appropriate response to the client
+        """
+        # use regex to both validate the data, and extract the information I care about
         pattern = re.compile(r'^([^ ]*) / HTTP/1.1([^ :]*) ?Host:([^ ]*) +([^ ]*) ?(.*)$')
-        print(data)
         match = pattern.fullmatch(data.decode())
-        print(match)
+        # make sure the data matches the expected format, if not reject the request
         if match:
+            # extract necessary data if request matches pattern
             _type, sub_sep, host, sep, body = match.groups()
-            print('type:', _type)
-            pattern = re.compile(r'(\d+\.\d+\.\d+\.\d+)/?(.*)')
-            _, file = pattern.fullmatch(host).groups()
+            # from the given host/filename, extract the given file and throw away the host
+            pattern = re.compile(r'\d+\.\d+\.\d+\.\d+/?(.*)')
+            # get the groups (tuple) and index the first (and only) group
+            file = pattern.fullmatch(host).groups()[0]
+            # if the request specifies a PUT type, call receive_file and exit function
             if _type == 'PUT':
                 return self.receive_file(file, body)
+            # if the request specifies a GET type, send the requested file to the client
             try:
+                # read data from given file if it exists
                 with open(file, 'rb') as f:
                     body_data = f.read()
+                # if data is successfully read, set the status of the header to 200
                 header = json.dumps({'status-code': 200}).encode()
             except FileNotFoundError:
+                # if file is not found, return a 404 error to notify the client
                 body_data = b'FILE NOT FOUND!'
                 header = json.dumps({'status-code': 404}).encode()
 
-
+            # separate the data into chunks (not necessary, see separate_chunks function)
             chunks = separate_chunks(header + sep.encode() + body_data)
-            print(chunks)
+            # send each chunk to the client
             for chunk in chunks:
                 self.sock.send(chunk)
+            # data sent, close connection
             self.sock.close()
+            # client connection closed, begin waiting for a new connection
             self.accept_connection()
         else:
-            print(data)
+            # invalid request given, notify client
             self.sock.send(b'Invalid request, closing connection. To retry, reconnect.')
+            # close connection to client
             self.sock.close()
+            # begin waiting for a new connection
             self.accept_connection()
 
-    def receive_file(self, file, body):
-        print('ran')
+    def receive_file(self, file: str, body: str) -> None:
+        """
+        :param file: the filename to store the data as
+        :param body: the data to store in the file
+
+        stores the given body data in the given filename
+        """
+        # make srec (server received) directory if it doesn't exist
         if not os.path.isdir('srec'):
             os.mkdir('srec')
+        # save data in srec directory with given filename
         if file:
             print(f'file has been saved as: srec/{file}')
             with open('srec/' + file, 'w') as f:
                 f.write(body)
         else:
             print('body:', body)
+        # close the connection
         self.sock.close()
 
 
-def separate_chunks(data, length=4096):
+def separate_chunks(data: Union[bytes, str],
+                    length: int = 4096
+                    ) -> List[Union[bytes, str]]:
+    """
+    :param data: the data to separate
+    :param length: the length each chunk should be
+    :return: the data given separated into chunks of the given length
+
+    I realized after I wrote this I can just send all the data I don't need to
+    separate it manually. But I already wrote this so it's staying.
+    """
     counter, chunks = 0, []
     while counter < len(data):
         chunks.append(data[counter:length])
@@ -97,6 +145,9 @@ def separate_chunks(data, length=4096):
 
 
 def main():
+    """
+    create a server object and accept the first connection
+    """
     s = Server()
     s.accept_connection()
 
